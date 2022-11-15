41cb585dd96495df0039e64a4a414eba148c9b64
ekqls3301 <72439207+ekqls3301@users.noreply.github.com>
2022-11-15 21:21:21 +0900

    Update model.js

--- model.js
+++ model.js
@@ -59,6 +59,8 @@ let URL = "./model/";
         console.log("init");
         //askNotificationPermission();
         //initCall();
+        var start_audio = new Audio('./audio/start.mp3');
+        start_audio.play();
         initCam();
 
     }
